116705c501c5bddb96325be3977f645f8bd8cd02
James Pritchard <james.edward.pritchard@gmail.com>
2022-02-03 20:21:48 -0500

    web3 connect wallet & webpack 5 config

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import './App.css';
-import { Button } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Web3 from 'web3';
+// import Web3 from 'web3';
 
-
-async function connectWallet(){
-  try{
-    if(window.ethereum){
-      var web3 = new Web3(window.ethereum);
-      await ethereum.request({ method: 'eth_requestAccounts' });
-      var accounts = accounts[0];
-      document.getElementById('wallet-address').textContent = accounts;
-
-      contract = new web3.eth.Contract(ABI, ADDRESS);
-      document.getElementById('mint').onclick = async ()=>{
-        var _mintAmount = Number(document.querySelector('[name=amount]').value);
-        var mintRate = Number(await contract.methods.cost().call());
-        var totalAmount = mintRate * _mintAmount;
-        contract.methods.mint(account, _mintAmount).send({from:account, value:(totalAmount)});
-      }
-    }
-  }catch(ex){
-    console.log(ex);
+const connectwallet = async () => {
+  // Check if MetaMask is installed on user's browser
+  if(window.ethereum) {
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    const chainId = await window.ethereum.request({ method: 'eth_chainId'});
+    // Check if user is connected to Mainnet
+    if(chainId !== '0x1') {
+      alert("Please connect to Mainnet");
+    } 
+    // else {
+    //   let wallet = accounts[0];
+    //   setWalletAddress(wallet);
+    // }
+  } else {
+    alert("Please install Mask");
   }
 }
+// async function connectWallet(){
+//   try{
+//     if(window.ethereum){
+//       var web3 = new Web3(window.ethereum);
+//       await ethereum.request({ method: 'eth_requestAccounts' });
+//       var accounts = accounts[0];
+//       document.getElementById('wallet-address').textContent = accounts;
+
+//       contract = new web3.eth.Contract(ABI, ADDRESS);
+//       document.getElementById('mint').onclick = async ()=>{
+//         var _mintAmount = Number(document.querySelector('[name=amount]').value);
+//         var mintRate = Number(await contract.methods.cost().call());
+//         var totalAmount = mintRate * _mintAmount;
+//         contract.methods.mint(account, _mintAmount).send({from:account, value:(totalAmount)});
+//       }
+//     }
+//   }catch(ex){
+//     console.log(ex);
+//   }
+// }
 
   function App() {
   return (
@@ -34,12 +49,12 @@ async function connectWallet(){
         </div>
         
           <form className="mintForm">
-              <Button name="buttonSubmit">Connect Wallet</Button>
+              <button className="buttonSubmit" onClick={connectwallet}>Connect Wallet</button>
             <div className="card" id="wallet-address">
-              <label className="floatingInput">0x2cDeC4E66b89102e66a3c5C119eD35168aD42312</label>
+              <label className="floatingInput">Address</label>
               <input  className="mintAmountInput" type="number" defaultValue={1} min={1} max={5}></input>
             </div>
-              <button class="buttonSubmit">Mint</button>
+              <button className="buttonSubmit">Mint</button>
           </form>
       </div>
     </div>
